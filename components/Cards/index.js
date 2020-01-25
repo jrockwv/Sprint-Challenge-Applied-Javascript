eacd36fc8b1b31bb8607bbcebfd9fca07c0ed7a6
john ruble <jrockwv@gmail.com>
2020-01-24 22:35:08 -0500

    final product

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,45 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(function (response) {
+    // console.log(response);
+    const articles = response.data.articles
+    const proto = Object.keys(articles)
+
+    proto.forEach(topic => {
+        articles[topic].forEach(article => {
+            const posts = newCards(article)
+            document.querySelector('.cards-container').append(posts)
+        })
+    })
+  })
+
+  function newCards(panels){
+    const cardDiv = document.createElement('div')
+    const headDiv = document.createElement('div')
+    const authorDiv = document.createElement('div')
+    const imgDiv = document.createElement('div')
+    const img = document.createElement('img')
+    const authorSpan = document.createElement('span')
+    
+    cardDiv.classList.add('card')
+    headDiv.classList.add('headline')
+    authorDiv.classList.add('author')
+    imgDiv.classList.add('img-container')
+
+    headDiv.textContent = panels.headline
+    authorSpan.textContent = panels.authorName
+
+    img.src = panels.authorPhoto
+
+    cardDiv.append(headDiv, authorDiv)
+    authorDiv.append(imgDiv, authorSpan)
+    imgDiv.append(img)
+
+    return cardDiv
+}
+
+
+
